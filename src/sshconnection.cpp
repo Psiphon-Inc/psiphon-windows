f44f996429956e7f56330889ff9ce6f0bc0e0640
Rod Hynes <rod-hynes@users.noreply.github.com>
2012-01-05 15:20:28 -0500

    Store SSH host key for each case (standard and obfuscated ports)

--- src/sshconnection.cpp
+++ src/sshconnection.cpp
@@ -238,11 +238,6 @@ bool SSHConnection::Connect(
 
     m_connectType = connectType;
 
-    // Add host to Plonk's known host registry set
-    // Note: currently we're not removing this after the session, so we're leaving a trace
-
-    SetPlonkSSHHostKey(sshServerAddress, sshServerPort, sshServerHostKey);
-
     // Start plonk using Psiphon server SSH parameters
 
     // Note: -batch ensures plonk doesn't hang on a prompt when the server's host key isn't
@@ -258,6 +253,11 @@ bool SSHConnection::Connect(
             return false;
         }
 
+        // Add host to Plonk's known host registry set
+        // Note: currently we're not removing this after the session, so we're leaving a trace
+
+        SetPlonkSSHHostKey(sshServerAddress, sshObfuscatedPort, sshServerHostKey);
+
         plonkCommandLine = m_plonkPath
                                 + _T(" -ssh -C -N -batch")
                                 + _T(" -P ") + sshObfuscatedPort
@@ -269,6 +269,10 @@ bool SSHConnection::Connect(
     }
     else
     {
+        // Add host to Plonk's known host registry set
+
+        SetPlonkSSHHostKey(sshServerAddress, sshServerPort, sshServerHostKey);
+
         plonkCommandLine = m_plonkPath
                                 + _T(" -ssh -C -N -batch")
                                 + _T(" -P ") + sshServerPort
