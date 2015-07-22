8909b02f5fb4971c3a12743dba0cf8fe055641c8
Adam Pritchard <pritchard.adam@gmail.com>
2015-07-22 16:01:53 -0400

    Made wordart on About a link to home page

--- src/webui/vendor/icomoon/ie7.js
+++ src/webui/vendor/icomoon/ie7.js
@@ -1,11 +1,6 @@
 /* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
 /* The script tag referencing this file must be placed before the ending body tag. */
 
-/* Use conditional comments in order to target IE 7 and older:
-	<!--[if lt IE 8]><!-->
-	<script src="ie7/ie7.js"></script>
-	<!--<![endif]-->
-*/
 
 (function() {
 	function addIcon(el, entity) {
