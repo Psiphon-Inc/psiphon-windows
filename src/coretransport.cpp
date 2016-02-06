d05e680f198f1dbab8497709686dff75853b0b67
Adam Pritchard <pritchard.adam@gmail.com>
2016-02-05 20:10:31 -0500

    Attmpt to determine current device region and pass to tunnel-core

--- src/coretransport.cpp
+++ src/coretransport.cpp
@@ -359,6 +359,7 @@ bool CoreTransport::WriteParameterFiles(tstring& configFilename, tstring& server
     config["DataStoreDirectory"] = WStringToUTF8(shortDataStoreDirectory);
     config["DataStoreTempDirectory"] = WStringToUTF8(shortTempPath);
     config["UseIndistinguishableTLS"] = true;
+    config["DeviceRegion"] = WStringToUTF8(GetDeviceRegion());
 
     // Don't use an upstream proxy when in VPN mode. If the proxy is on a private network,
     // we may not be able to route to it. If the proxy is on a public network we prefer not
