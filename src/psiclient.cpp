c16f4a9083d8c2d2105f6d0d51b878978a4d3e68
Kruger <5760099+adamkruger@users.noreply.github.com>
2017-02-10 15:42:20 -0500

    Trigger a notification (with a balloon) at an interval while connected, launch the homepage when the balloon is clicked.

--- src/psiclient.cpp
+++ src/psiclient.cpp
@@ -66,6 +66,7 @@ static bool g_htmlUiFinished = false;
 // Note: Trying to use SetTimer/KillTimer without an explicit ID led to inconsistent behaviour.
 #define TIMER_ID_SYSTRAY_MINIMIZE       100
 #define TIMER_ID_SYSTRAY_STATE_UPDATE   101
+#define TIMER_ID_CONNECTED_REMINDER     102
 
 
 //==== Controls ================================================================
@@ -120,16 +121,18 @@ void OnCreate(HWND hWndParent)
 
 //==== String Table helpers ==================================================
 
-#define STRING_KEY_STATE_STOPPED_TITLE          "appbackend#state-stopped-title"
-#define STRING_KEY_STATE_STOPPED_BODY           "appbackend#state-stopped-body"
-#define STRING_KEY_STATE_STARTING_TITLE         "appbackend#state-starting-title"
-#define STRING_KEY_STATE_STARTING_BODY          "appbackend#state-starting-body"
-#define STRING_KEY_STATE_CONNECTED_TITLE        "appbackend#state-connected-title"
-#define STRING_KEY_STATE_CONNECTED_BODY         "appbackend#state-connected-body"
-#define STRING_KEY_STATE_STOPPING_TITLE         "appbackend#state-stopping-title"
-#define STRING_KEY_STATE_STOPPING_BODY          "appbackend#state-stopping-body"
-#define STRING_KEY_MINIMIZED_TO_SYSTRAY_TITLE   "appbackend#minimized-to-systray-title"
-#define STRING_KEY_MINIMIZED_TO_SYSTRAY_BODY    "appbackend#minimized-to-systray-body"
+#define STRING_KEY_STATE_STOPPED_TITLE              "appbackend#state-stopped-title"
+#define STRING_KEY_STATE_STOPPED_BODY               "appbackend#state-stopped-body"
+#define STRING_KEY_STATE_STARTING_TITLE             "appbackend#state-starting-title"
+#define STRING_KEY_STATE_STARTING_BODY              "appbackend#state-starting-body"
+#define STRING_KEY_STATE_CONNECTED_TITLE            "appbackend#state-connected-title"
+#define STRING_KEY_STATE_CONNECTED_BODY             "appbackend#state-connected-body"
+#define STRING_KEY_STATE_CONNECTED_REMINDER_TITLE   "appbackend#state-connected-reminder-title"
+#define STRING_KEY_STATE_CONNECTED_REMINDER_BODY    "appbackend#state-connected-reminder-body"
+#define STRING_KEY_STATE_STOPPING_TITLE             "appbackend#state-stopping-title"
+#define STRING_KEY_STATE_STOPPING_BODY              "appbackend#state-stopping-body"
+#define STRING_KEY_MINIMIZED_TO_SYSTRAY_TITLE       "appbackend#minimized-to-systray-title"
+#define STRING_KEY_MINIMIZED_TO_SYSTRAY_BODY        "appbackend#minimized-to-systray-body"
 
 static map<string, wstring> g_stringTable;
 
@@ -483,6 +486,52 @@ static void UpdateSystrayConnectedState()
         // Just update the state now.
         UpdateSystrayConnectedStateHelper();
     }
+}
+
+UINT CONNECTED_REMINDER_INTERVAL_MS = 5 * 60 * 1000;
+static UINT_PTR g_showConnectedReminderBalloonTimerID = 0;
+static VOID CALLBACK ShowConnectedReminderBalloonTimer(HWND hWnd, UINT, UINT_PTR idEvent, DWORD);
+
+static void StartConnectedReminderTimer()
+{
+    g_showConnectedReminderBalloonTimerID = ::SetTimer(
+        g_hWnd,
+        TIMER_ID_CONNECTED_REMINDER,
+        CONNECTED_REMINDER_INTERVAL_MS,
+        ShowConnectedReminderBalloonTimer);
+}
+
+static void StopConnectedReminderTimer()
+{
+    ::KillTimer( g_hWnd, TIMER_ID_CONNECTED_REMINDER);
+}
+
+static void ShowConnectedReminderBalloon()
+{
+    if (g_htmlUiFinished)
+    {
+        return;
+    }
+
+    if (CONNECTION_MANAGER_STATE_CONNECTED == g_connectionManager.GetState())
+    {
+        HICON hIcon = g_notifyIconConnected;
+        wstring infoTitle, infoBody;
+        GetStringTableEntry(STRING_KEY_STATE_CONNECTED_REMINDER_TITLE, infoTitle);
+        GetStringTableEntry(STRING_KEY_STATE_CONNECTED_REMINDER_BODY, infoBody);
+        UpdateSystrayIcon(hIcon, infoTitle, infoBody);
+    }
+}
+
+static VOID CALLBACK ShowConnectedReminderBalloonTimer(HWND hWnd, UINT, UINT_PTR idEvent, DWORD)
+{
+    assert(g_showConnectedReminderBalloonTimerID == idEvent);
+    ::KillTimer(hWnd, idEvent);
+    g_showConnectedReminderBalloonTimerID = 0;
+
+    ShowConnectedReminderBalloon();
+
+    StartConnectedReminderTimer();
 }
 
 
@@ -843,6 +892,7 @@ void UI_SetStateStopped()
 void UI_SetStateStopping()
 {
     UpdateSystrayConnectedState();
+    StopConnectedReminderTimer();
 
     Json::Value json;
     json["state"] = "stopping";
@@ -866,6 +916,7 @@ void UI_SetStateStarting(const tstring& transportProtocolName)
 void UI_SetStateConnected(const tstring& transportProtocolName, int socksPort, int httpPort)
 {
     UpdateSystrayConnectedState();
+    StartConnectedReminderTimer();
 
     Json::Value json;
     json["state"] = "connected";
@@ -1307,8 +1358,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         return HandleNotify(hWnd, (NMHDR*)lParam);
 
     case WM_PSIPHON_TRAY_ICON_NOTIFY:
+        if (lParam == NIN_BALLOONUSERCLICK &&
+            CONNECTION_MANAGER_STATE_CONNECTED == g_connectionManager.GetState())
+        {
+            g_connectionManager.OpenHomePages(INFO_LINK_URL, false);
+        }
         // Restore/foreground the app on any kind of click
-        if (lParam == WM_LBUTTONUP || 
+        else if (lParam == WM_LBUTTONUP || 
             lParam == WM_LBUTTONDBLCLK ||
             lParam == WM_RBUTTONUP ||
             lParam == WM_RBUTTONDBLCLK ||
